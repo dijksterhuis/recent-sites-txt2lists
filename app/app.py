35e4def560e0894782c3fb073723df4e2b2969ec
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-19 15:42:41 +0000

    move to native flask app for ElasticBeanstalk

--- app/app.py
+++ app/app.py
@@ -28,4 +28,4 @@ def edit():
     return('empty.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True,port=100)
+	app.run(host='0.0.0.0',debug=True,port=80)
--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-docker build --no-cache -t dijksterhuis/recent-sites:latest ./
-docker push dijksterhuis/recent-sites:latest
