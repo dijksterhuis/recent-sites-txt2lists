341a1a7f5af6f4c2904365d4fb8ad0d5a21cf92d
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-19 15:52:24 +0000

    remove redis from master, move app to correct place

--- redis/ETL/docker_run.sh
+++
@@ -1,7 +0,0 @@
-path = '/Users/Mike/postgrad/Github/recent-sites-txt2lists'
-cp /Users/Mike/Desktop/sites/*.txt $path/app/data/
-
-docker run \
-	-it --rm --name test --network recent-sites \
-	-v $path/app/data:/home/data \
-	recent-sites-redisload:0.1 /bin/ash -c 'python /home/load_redis.py'
--- redis/docker_run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --rm --network recent-sites --name recent-sites-redis redis:alpine redis-server
