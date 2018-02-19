44d25031d4be7c6aa7d5a488020d27b25acff179
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-19 16:25:43 +0000

    app to application all

--- application.py
+++ application.py
@@ -5,27 +5,26 @@ from get_links_from_txt_files import get_data
 # -- get site name etc.
 
 application = Flask(__name__)
-app = application
 
-@app.route('/')
+@application.route('/')
 def index():
     d = get_data()
     categories = [ k for k in d.keys() ]
     if len(categories) >= 1: return render_template('list.html',data_dict=d, cats=categories) 
     else: return render_template('empty.html')
 
-@app.route('/add-one')
+@application.route('/add-one')
 def add_one():
     return render_template('empty.html')
 
-@app.route('/add-many')
+@application.route('/add-many')
 def add_many():
     return render_template('empty.html')
 
-@app.route('/edit')
+@application.route('/edit')
 def edit():
     return render_template('empty.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
