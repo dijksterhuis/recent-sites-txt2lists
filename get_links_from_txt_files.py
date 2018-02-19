4d62dacfb3f006b60658565645dd486761353aec
dijksterhuis <mrobeson@dundee.ac.uk>
2018-02-19 17:42:25 +0000

    update

--- get_links_from_txt_files.py
+++ get_links_from_txt_files.py
@@ -1,15 +1,15 @@
 def get_data(filepath='/home/ec2-user/'):
-	import os
-	d, files_list = dict(), os.listdir(filepath)
-	if len(files_list) < 1:
-		print('No files exist! Exiting')
-		exit()
-	else:
-		for filename in os.listdir(filepath):
-			if '.txt' in filename:
-				with open(filepath + filename,'r') as f:
-					l  = [ line.rstrip('\n\r') for line in f.readlines() ]
-				l.reverse()
-				d[filename] = l
-	return d
+    import os
+    d, files_list = dict(), os.listdir(filepath)
+    if len(files_list) < 1:
+        print('No files exist! Exiting')
+        exit()
+    else:
+        for filename in os.listdir(filepath):
+            if '.txt' in filename:
+                with open(filepath + filename,'r') as f:
+                    l  = [ line.decode('utf-8').rstrip('\n\r') for line in f.readlines() ]
+                l.reverse()
+                d[filename] = l
+    return d
 
